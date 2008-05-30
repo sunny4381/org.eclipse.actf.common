a07a08dcb6642d62d85f2b356fd86cb9ede5bf88
kfukuda <kfukuda>
2008-05-30 08:19:38 +0000

    [232282] Refactoring Flash Bridge

--- plugins/org.eclipse.actf.model.flash/src/org/eclipse/actf/model/flash/util/FlashAdjust.java
+++ plugins/org.eclipse.actf.model.flash/src/org/eclipse/actf/model/flash/util/FlashAdjust.java
@@ -13,17 +13,15 @@
 
 package org.eclipse.actf.model.flash.util;
 
+import org.eclipse.actf.model.flash.ASNode;
 import org.eclipse.actf.model.flash.IFlashConst;
 import org.eclipse.actf.model.flash.IFlashPlayer;
-import org.eclipse.actf.util.win32.comclutch.IDispatch;
 
 public class FlashAdjust {
 
-	private IDispatch idispFlash = null;
-	IFlashPlayer player = null;
+	private IFlashPlayer player = null;
 
 	public FlashAdjust(IFlashPlayer flashPlayer) {
-		idispFlash = flashPlayer.getDispatch();
 		player = flashPlayer;
 	}
 
@@ -33,49 +31,34 @@ public class FlashAdjust {
 	}
 
 	public void adjust(String newId) {
-		if (idispFlash == null)
-			return;
-		Object o1 = null, o2 = null;
-		try {
-			o1 = idispFlash.invoke(IFlashConst.PLAYER_GET_VARIABLE,
-					new Object[] { IFlashConst.PATH_ROOTLEVEL
-							+ IFlashConst.PATH_IS_AVAILABLE });
-		} catch (Exception e) {
-			// e.printStackTrace();
-		}
-		try {
-			o2 = idispFlash.invoke(IFlashConst.PLAYER_GET_VARIABLE,
-					new Object[] { IFlashConst.PATH_BRIDGELEVEL
-							+ IFlashConst.PATH_IS_AVAILABLE });
-
-		} catch (Exception e) {
-			// e.printStackTrace();
-		}
-		if (o1 != null || o2 != null) {
-			setErrorAttribute(IFlashConst.ERROR_OK + "Flash DOM detected"); //$NON-NLS-1$
+		if(null==player){
 			return;
 		}
-
-		String tagName = (String) idispFlash.get("tagName");
-		if (!"OBJECT".equals(tagName)) {
+		
+		String tagName = player.getPlayerProperty("tagName");
+		if (!"OBJECT".equalsIgnoreCase(tagName)) {
 			setErrorAttribute(IFlashConst.ERROR_NG + tagName
 					+ " tag is not supoported"); //$NON-NLS-1$ //$NON-NLS-2$
 			return;
 		}
 
-		Integer readyState = (Integer) idispFlash.get("ReadyState");
-		if (readyState != null) {
-			if (readyState < 4) {
-				setErrorAttribute(IFlashConst.ERROR_WAIT
-						+ "Flash movie is not ready (ReadyState=" + readyState + ")"); //$NON-NLS-1$ //$NON-NLS-2$
-				return;
-			}
+		ASNode rootNode = player.getRootNode();
+		if (null != rootNode) {
+			setErrorAttribute(IFlashConst.ERROR_OK + "Flash DOM detected"); //$NON-NLS-1$
+			return;
+		}
+
+		if (!player.isReady()) {
+			setErrorAttribute(IFlashConst.ERROR_WAIT
+					+ "Flash movie is not ready"); //$NON-NLS-1$ //$NON-NLS-2$
+			return;
 		}
 
 		setErrorAttribute(IFlashConst.ERROR_NA + "Flash DOM is not available"); //$NON-NLS-1$
 	}
 
 	private void setErrorAttribute(String message) {
+		//System.out.println(message);
 		player.setPlayerProperty(IFlashConst.ATTR_ERROR, message);
 	}
 }
