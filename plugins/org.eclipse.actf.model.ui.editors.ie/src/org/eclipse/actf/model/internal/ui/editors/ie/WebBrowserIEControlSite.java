5ac97fc88612dda9d15ba373953dddc9fe053ff6
Kentarou Fukuda <kentarou@jp.ibm.com>
2016-06-08 00:11:50 +0900

    Bug 495607 - Crash in ACTF browser when 'ActiveX Filter' is enabled in IE11

--- plugins/org.eclipse.actf.model.ui.editors.ie/src/org/eclipse/actf/model/internal/ui/editors/ie/WebBrowserIEControlSite.java
+++ plugins/org.eclipse.actf.model.ui.editors.ie/src/org/eclipse/actf/model/internal/ui/editors/ie/WebBrowserIEControlSite.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007 IBM Corporation and Others
+ * Copyright (c) 2007, 2016 IBM Corporation and Others
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *    Takashi ITOH - initial API and implementation
+ *    Kentarou FUKUDA - 495607
  *******************************************************************************/
 
 package org.eclipse.actf.model.internal.ui.editors.ie;
@@ -468,7 +469,7 @@ public class WebBrowserIEControlSite extends OleControlSite {
 			ignoreNextMessage = true;
 		}
 
-		if (dwAction == URLACTION_ACTIVEX_RUN) {
+		if (dwAction == URLACTION_ACTIVEX_RUN && pContext != 0) {
 			GUID guid = new GUID();
 			COM.MoveMemory(guid, pContext, GUID.sizeof);
 			if (COM.IsEqualGUID(guid, COM.IIDJavaBeansBridge)
